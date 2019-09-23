69a8799664437019cbc15cd0003a9ba5ffdd7931
DANNY BERNARD <danny.bernard@hotmail.com>
2019-09-22 22:34:13 -0400

    Empezamos a preparar

---
+++ BLL/RepositorioBase.cs
@@ -0,0 +1,60 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RepositorioBase<T> : IDisposable, IRepository<T> where T : class
+    {
+        
+        internal Contexto _contexto;
+
+        public RepositorioBase()
+        {
+            _contexto = new Contexto();
+        }
+       
+        public T Buscar(int id)
+        {
+            T entity;
+            try
+            {
+                entity = _contexto.
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Guardar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Modificar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : Context
+    {
+        //public DbSet<> MyProperty { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
